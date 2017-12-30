aad8f3cebc1e30bab8393e98d24914721f049245
Nud <nudemeth@hotmail.com>
2017-12-30 12:46:06 +0700

    Refactor WebServer to use sys.addShutdownHook instead

--- src/main/scala/com/nudemeth/example/server/WebServer.scala
+++ src/main/scala/com/nudemeth/example/server/WebServer.scala
@@ -2,16 +2,16 @@ package com.nudemeth.example.server
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.ServerBinding
 import akka.stream.ActorMaterializer
 
-import scala.concurrent.{ExecutionContextExecutor, Future}
-import scala.io.StdIn
+import scala.concurrent.{Await, ExecutionContextExecutor}
+import scala.util.{Failure, Success}
+import scala.concurrent.duration._
 
 object WebServer extends App {
   private val server = WebServer()
   server.start()
-  StdIn.readLine() // let it run until user presses return
-  server.stop()
 }
 
 final case class WebServer() extends ServerRoutes {
@@ -19,18 +19,34 @@ final case class WebServer() extends ServerRoutes {
   private implicit val materializer: ActorMaterializer = ActorMaterializer()
   private implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
-  private var server: Future[Http.ServerBinding] = _
-
+  val bindingAddress = "0.0.0.0"
+  val bindingPort = 8080
+  val shutdownTimeout = 60
 
   def start(): Unit = {
-    server = Http().bindAndHandle(routes, "0.0.0.0", 8080)
-    log.info(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    log.info(s"Starting server on $bindingAddress:$bindingPort")
+    Http().bindAndHandle(routes, bindingAddress, bindingPort)
+      .onComplete {
+        case Success(binding) =>
+          val address = binding.localAddress
+          registerShutdownHook(binding)
+          log.info(s"Server is listening on ${address.getHostString}:${address.getPort}")
+        case Failure(ex) =>
+          log.error("Server could not be started", ex)
+          stop()
+      }
   }
 
   override def stop(): Unit = {
+    log.info("Server is being shut down")
     super.stop()
-    server
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
+    system.terminate()
+    Await.result(system.whenTerminated, shutdownTimeout seconds)
+  }
+
+  private def registerShutdownHook(binding: ServerBinding): Unit = {
+    sys.addShutdownHook {
+      binding.unbind().onComplete( _ => stop())
+    }
   }
 }
